409e122fc0842e57263da64c18f1daf541cbb1b3
Gavin <48301286+letitgone@users.noreply.github.com>
2021-08-10 19:05:56 +0800

    Gavin:Code

--- multilingual/java/HelloJava.java
+++ multilingual/java/HelloJava.java
@@ -1,5 +1,7 @@
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello Java!");
+        String javaVersion = System.getProperty("java.version");
+        System.out.println(javaVersion);
     }
 }
--- multilingual/python/hello_python.py
+++ multilingual/python/hello_python.py
@@ -1,1 +1,4 @@
+import sys
+
 print('Hello Python!')
+print("Python Version {}".format(str(sys.version).replace('\n', '')))
