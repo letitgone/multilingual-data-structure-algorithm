7987dccb8b8496f0d5cd1223b81401c1a5e5e44d
ZhangGJ <48301286+letitgone@users.noreply.github.com>
2021-08-16 07:52:10 +0800

    Gavin:c++11

--- multilingual/c++/hello_c++.cpp
+++ multilingual/c++/hello_c++.cpp
@@ -5,5 +5,6 @@ int main()
     std::cout << "Hello C++!" << std::endl;
     // c++11
     std::cout << R"(Jim "King" Tutt uses "\n" instead of endl.)" << '\n';
+    std::cout << R"+*("(Who wouldn't?)", she whispered.)+*" << std::endl;
     return 0;
 }
