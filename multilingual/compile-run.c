d77beba7bf2ad3d5d55a44cf7a8319a35d1c3635
Gavin <48301286+letitgone@users.noreply.github.com>
2021-08-17 19:20:01 +0800

    Gavin:File

--- multilingual/compile-run.c
+++ multilingual/compile-run.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// #define FILENAME ".multilingual"
+#define FILENAME "/test.txt"
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2) {
-        printf("Arguments error!");
-        return 1;
-    }
+    // printf("argc: %d\n", argc);
+    // printf("argv: %s\n", argv[0]);
+    // printf("argv: %s\n", argv[1]);
+    // if (argc != 2)
+    // {
+    //     printf("Arguments error!");
+    //     return 1;
+    // }
     // argv[1]
-    printf("argc: %d\n", argc);
-    printf("argv: %s\n", argv[0]);
-    printf("argv: %s\n", argv[1]);
+    // "multilingual-data-structure-algorithm"
+    FILE *file;
+    if ((file = fopen(FILENAME, "r")) == NULL)
+    {
+        file = fopen(FILENAME, "w");
+        // printf("Cant't open %s\n", FILENAME);
+        // return EXIT_FAILURE;
+    }
+    // char ch;
+    // while ((ch = getc(file)) != EOF)
+    // {
+    //     ch += ch;
+    // }
+    fclose(file);
+
     return 0;
 }
