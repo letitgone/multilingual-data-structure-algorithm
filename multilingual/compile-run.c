40d81e306c6aefb79af5c9f33e3a5ab446c627dd
Gavin <48301286+letitgone@users.noreply.github.com>
2021-08-17 14:26:40 +0800

    Gavin:Console

--- multilingual/compile-run.c
+++ multilingual/compile-run.c
@@ -2,5 +2,13 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc != 2) {
+        printf("Arguments error!");
+        return 1;
+    }
+    // argv[1]
+    printf("argc: %d\n", argc);
+    printf("argv: %s\n", argv[0]);
+    printf("argv: %s\n", argv[1]);
     return 0;
 }
