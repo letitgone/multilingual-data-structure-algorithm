9ad63e788f6dce259c4feb6eabb117633078e50a
Gavin <48301286+letitgone@users.noreply.github.com>
2021-08-19 18:49:08 +0800

    Gavin:C

--- multilingual/compile-run.c
+++ multilingual/compile-run.c
@@ -1,34 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// #define FILENAME ".multilingual"
-#define FILENAME "/test.txt"
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <errno.h>
+#include <string.h>
+
+#define URL_FILENAME "/Users/zhanggj/Desktop/multilingual.conf"
+
+#define ROOT_DIR "/"
+
+#define PROJECT_NAME "multilingual-data-structure-algorithm"
+
+// char *search_file(char *target_file, char file_or_dir);
 
 int main(int argc, char *argv[])
 {
-    // printf("argc: %d\n", argc);
-    // printf("argv: %s\n", argv[0]);
-    // printf("argv: %s\n", argv[1]);
-    // if (argc != 2)
-    // {
-    //     printf("Arguments error!");
-    //     return 1;
-    // }
-    // argv[1]
-    // "multilingual-data-structure-algorithm"
     FILE *file;
-    if ((file = fopen(FILENAME, "r")) == NULL)
-    {
-        file = fopen(FILENAME, "w");
-        // printf("Cant't open %s\n", FILENAME);
-        // return EXIT_FAILURE;
-    }
-    // char ch;
-    // while ((ch = getc(file)) != EOF)
-    // {
-    //     ch += ch;
-    // }
-    fclose(file);
 
-    return 0;
+    char compiler_url[100];
+
+    if ((access(URL_FILENAME, F_OK) == -1))
+    {
+        file = fopen(URL_FILENAME, "w");
+        if (file == NULL)
+        {
+            printf("Open %s error!\n", URL_FILENAME);
+        }
+
+        char *target_file;
+
+        struct dirent *ptr;
+
+        DIR *dir = opendir(ROOT_DIR);
+        while ((ptr = readdir(dir)) != NULL)
+        {
+            if (strcmp(ptr->d_name, PROJECT_NAME) == 0)
+            {
+                printf("");
+            }
+        }
+    }
+    else
+    {
+        file = fopen(URL_FILENAME, "r");
+        while (fgets(compiler_url, 100, file) != NULL)
+            ;
+        printf("URL: %s", compiler_url);
+    }
+
+    fclose(file);
+    return EXIT_SUCCESS;
 }
+
+// char *search_file(char *target_file, char file_or_dir)
+// {
+//     fread(target_file,)
+// }
