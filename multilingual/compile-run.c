87db0dc37cd29a42ccb4e3592946ce8125a4744d
Gavin <48301286+letitgone@users.noreply.github.com>
2021-08-27 15:19:21 +0800

    Gavin:File

--- multilingual/compile-run.c
+++ multilingual/compile-run.c
@@ -9,11 +9,11 @@
 
 #define URL_FILENAME "/Users/zhanggj/Desktop/multilingual.conf"
 
-#define ROOT_DIR "/"
+#define ROOT_DIR "/Users/zhanggj/Downloads"
 
 #define PROJECT_NAME "multilingual-data-structure-algorithm"
 
-char *search_file(char *target_file, char *file_dir);
+char *search_file(char *target_file, char *file_dir, int file_dir_length);
 
 char *file_suffix(char *filename);
 
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 
     if ((access(URL_FILENAME, F_OK) == -1))
     {
-        char *multilingual_url = search_file(PROJECT_NAME, ROOT_DIR);
+        char *multilingual_url = search_file(PROJECT_NAME, ROOT_DIR, strlen(ROOT_DIR));
         file = fopen(multilingual_url, "w");
         if (file == NULL)
         {
@@ -50,7 +50,7 @@ int main(int argc, char *argv[])
         if (compiler_url[0] != '\0')
         {
             char *file_name = argv[1];
-            char *result_url = search_file(file_name, compiler_url);
+            char *result_url = search_file(file_name, compiler_url, 1);
             char *command_cd = strcat("cd ", result_url);
             system(command_cd);
             char *suffix = file_suffix(file_name);
@@ -86,36 +86,32 @@ int main(int argc, char *argv[])
     return EXIT_SUCCESS;
 }
 
-char *search_file(char *target_file, char *file_dir)
+char *search_file(char *target_file, char *file_dir, int file_dir_length)
 {
-
     struct dirent *ptr;
-
     char *multilingual_url = NULL;
-
     struct stat st;
-
     DIR *dir = opendir(file_dir);
-    while ((ptr = readdir(dir)) != NULL)
+    while (dir != NULL && (ptr = readdir(dir)) != NULL)
     {
         char *ptr_name = ptr->d_name;
+//        if (strcmp(ptr_name, "authserver") == 0)
+//        {
+//            printf("123");
+//        }
         if (strcmp(ptr_name, ".") == 0 || strcmp(ptr_name, "..") == 0)
         {
             continue;
         }
-        char ss[20] = "";
-        if (strcmp(file_dir, ROOT_DIR) == 0)
+        char ss[1024] = "";
+        strcpy(ss, file_dir);
+        char *last_char = file_dir + (file_dir_length - 1);
+        if (strcmp(last_char, "/") != 0)
         {
-            strcpy(ss, file_dir);
-            strcat(ss, ptr_name);
+            strcat(ss, "/");
         }
-        else
-        {
-            strcpy(ss, file_dir);
-            strcat(ss, ROOT_DIR);
-            strcat(ss, ptr_name);
-        }
-        stat(file_dir, &st);
+        strcat(ss, ptr_name);
+        stat(ss, &st);
         if (!S_ISDIR(st.st_mode))
         {
             continue;
@@ -123,18 +119,24 @@ char *search_file(char *target_file, char *file_dir)
         printf("d_name : %s\n", ptr_name);
         if (strcmp(ptr_name, target_file) == 0)
         {
-            multilingual_url = file_dir;
-            strcat(multilingual_url, target_file);
-            return multilingual_url;
+            return (char *) ss;
         }
         else
         {
-            char *result = search_file(PROJECT_NAME, ss);
+            char *result = search_file(PROJECT_NAME, ss, 1);
             if (result == NULL)
             {
                 continue;
             }
+            else
+            {
+                return result;
+            }
         }
+    }
+    if (dir != NULL)
+    {
+        closedir(dir);
     }
     return NULL;
 }
