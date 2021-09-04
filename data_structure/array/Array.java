a619126f671df35ec7b2c946e793d5caa3cdc52b
Gavin <GavinZhangGJ@163.com>
2021-09-04 11:32:52 +0800

    Gavin:test

--- data_structure/array/Array.java
+++ data_structure/array/Array.java
@@ -2,5 +2,7 @@ public class Array {
     public static void main(String[] args) {
 		int i = 3;
         System.out.println("Hello Java");
+        boolean = true;
+        System.out.println("test");
     }
 }
